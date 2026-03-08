5aace559eef89afd1a847527a504257c68fe2e4a
RxTuathade <maulana.reksi27@gmail.com>
2026-03-08 12:20:02 +0700

    Tambah kode vuln

---
+++ app.py
@@ -0,0 +1,10 @@
+# ❌ VULN: Menyimpan rahasia langsung di dalam teks kode (Hardcoded Secret)
+def connect_to_server():
+    private_key = """-----BEGIN RSA PRIVATE KEY-----
+    MIIEpQIBAAKCAQEA3Tz2mr7SZiAMfQyA2X8Zz1... (kode rahasia palsu) ...
+    -----END RSA PRIVATE KEY-----"""
+    
+    print("Menghubungkan ke server menggunakan kunci rahasia...")
+    # Proses koneksi...
+
+connect_to_server()
